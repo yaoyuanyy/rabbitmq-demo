ed8ca652e893c8b42d7dc6affad88766a6a42972
yaoliang(姚亮.企业云事业部.基础研发部) <yaoliang@ds.gome.com.cn>
2016-12-22 20:13:19 +0800

    first cm

--- rabbitmq-server/src/main/java/com/yy/Send.java
+++ rabbitmq-server/src/main/java/com/yy/Send.java
@@ -25,21 +25,5 @@ public class Send {
 
         channel.close();
         connection.close();
-        log();
-    }
-
-    public static void log() {
-        Throwable ex = new Throwable();
-        StackTraceElement[] stackElements = ex.getStackTrace();
-
-        if (stackElements != null) {
-            for (int i = 0; i < stackElements.length; i++) {
-                System.out.println(stackElements[i].getClassName());//返回类的完全限定名，该类包含由该堆栈跟踪元素所表示的执行点。
-                System.out.println(stackElements[i].getFileName());//返回源文件名，该文件包含由该堆栈跟踪元素所表示的执行点。
-                System.out.println(stackElements[i].getLineNumber());//返回源行的行号，该行包含由该堆栈该跟踪元素所表示的执行点。
-                System.out.println(stackElements[i].getMethodName());//返回方法名，此方法包含由该堆栈跟踪元素所表示的执行点。
-                System.out.println("-------------第"+i+"级调用-------------------");
-            }
-        }
     }
 }
