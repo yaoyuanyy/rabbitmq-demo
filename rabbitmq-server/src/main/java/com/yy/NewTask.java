a0a5eee32abf76c48937e1bafd9e86caa033dcc9
yaoliang(姚亮.企业云事业部.基础研发部) <yaoliang@ds.gome.com.cn>
2016-12-29 21:04:53 +0800

    cm

--- rabbitmq-server/src/main/java/com/yy/NewTask.java
+++ rabbitmq-server/src/main/java/com/yy/NewTask.java
@@ -9,8 +9,8 @@ import java.util.concurrent.TimeoutException;
 /**
  * Created by yaoliang on 2016/12/13.
  */
-public class Send {
-    private final static String QUEUE_NAME = "hello";
+public class NewTask {
+    private final static String QUEUE_NAME = "hello2";
 
     public static void main(String[] argv) throws java.io.IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
@@ -18,12 +18,26 @@ public class Send {
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        String message = "Hello World!";
+        String message = getMessage(argv);
         channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
         System.out.println(" [x] Sent '" + message + "'");
 
         channel.close();
         connection.close();
+    }
+    private static String getMessage(String[] strings){
+        if (strings.length < 1)
+            return "Hello World!";
+        return joinStrings(strings, " ");
+    }
+
+    private static String joinStrings(String[] strings, String delimiter) {
+        int length = strings.length;
+        if (length == 0) return "";
+        StringBuilder words = new StringBuilder(strings[0]);
+        for (int i = 1; i < length; i++) {
+            words.append(delimiter).append(strings[i]);
+        }
+        return words.toString();
     }
 }
